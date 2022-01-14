bb71b2c504d03e90551298545cffb066784fc953
Mithun G <mithun162001@gmail.com>
2022-01-14 16:16:04 +0530

    Add files via upload

---
+++ Streamlit/app2.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# Creating title
+st.title("This is a title")            # displays the title
+
+# Creating headers
+st.header("This is a header")         # displays text in header formatting
+
+# Creating markdown
+st.markdown("This is a markdown")
+
+# Creating a text
+st.text("This is a text")
+
+# Creating a success message
+st.success("Success")
+
+# Creating a warning message
+st.warning("Warning")
+
+# Creating a error message
+st.error("This is a error")
